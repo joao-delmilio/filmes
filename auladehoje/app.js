75b443367f3fd1b459b5def3a16077d26e952fd8
joao-delmilio <joao.delmilio14@gmail.com>
2025-06-10 19:52:58 -0300

    adicionando mais um filme

--- auladehoje/app.js
+++ auladehoje/app.js
@@ -2,7 +2,8 @@ let filme1 = {
     titulo: "Jurassic Park 0",
     foto: "filme1.webp",
     avaliacao: "10/10",
-    duracao: "1h50m"
+    duracao: "1h50m",
+    link: "https://netflix.com"
 }
 
 let filme2 = {
@@ -10,13 +11,15 @@ let filme2 = {
     foto: "filme2.jpg",
     avaliacao: "6/10",
     duracao: "2h10m",
+    link: "https://whatsapp.com"
 }
 
 let filme3 = {
     titulo: "Lilo e Stitch",
     foto: "filme3.jpg",
     avaliacao: "7/10",
-    duracao: "1h50m"
+    duracao: "1h50m",
+    link: "https://facebook.com"
 }
 
 let filme4 = {
@@ -24,6 +27,15 @@ let filme4 = {
     foto: "filme4.jpg",
     avaliacao: "5/10",
     duracao: "2h40m",
+    link: "https://instagram.com"
+}
+
+let filme5 = {
+    titulo:"Alien: Romulus",
+    foto: "filme5.jpg",
+    avaliacao: "8/10",
+    duracao: "1h 59m",
+    link: "https://www.rottentomatoes.com/m/alien_romulus",
 }
 
 function fnMontaCartao(filmeAtual) {
@@ -32,6 +44,7 @@ function fnMontaCartao(filmeAtual) {
             <img src="img/${filmeAtual.foto}">
             <h3>${filmeAtual.titulo}</h3>
             <span>⭐ ${filmeAtual.avaliacao}</span>
+            <a href="${filmeAtual.link}" target="_blank">Acesse:</a>
         `
 }
 
@@ -39,3 +52,4 @@ fnMontaCartao(filme1)
 fnMontaCartao(filme2)
 fnMontaCartao(filme3)
 fnMontaCartao(filme4)
+fnMontaCartao(filme5)
