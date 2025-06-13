dbb98a8bf89f5862141d4c92f75004e3eb15c71f
João Pedro Delmilio <joao.delmilio14@gmail.com>
2025-06-12 21:20:58 -0300

    Update app2.js

--- auladehoje/app2.js
+++ auladehoje/app2.js
@@ -1,9 +1,16 @@
 async function fnPegarFilmes(){
     let filmes = await fetch("dados-filmes.json")
-    console.dir(filmes)
-    let filmesTratados = await filmes.text()
-    console.dir(filmesTratados)
+    let filmesTratados = await filmes.json()
+
+    filmesTratados.forEach((filmeAtual) => {
+        document.querySelector(".lista-filmes").innerHTML += `
+        <div class="card-filme">
+            <img src="${filmeAtual.foto}"
+            <h3>${filmeAtual.titulo}</h3>
+            <span>${filmeAtual.avaliacao}</span>
+        </div>
+            `
+        })
 }
 
 fnPegarFilmes()
-console.log("return")
